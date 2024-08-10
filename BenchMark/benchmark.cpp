3b7608e6b8a51ea540c8e284cbddf18f2f962d9e
developerNoman <noman.shafique@rakurai.io>
2024-08-11 00:34:02 +0500

    The enable and disable feature of unittest and benchmark is added

--- BenchMark/benchmark.cpp
+++ BenchMark/benchmark.cpp
@@ -1,12 +1,44 @@
 #include <benchmark/benchmark.h>
 #include "codility.h"
+#include <vector>
+#include <algorithm>
 
-
-static void BM_BinaryGap(benchmark::State& state) {
+static void BM_BinaryGap_Small(benchmark::State& state) {
     for (auto _ : state) {
-        codility::binaryGap(1041);
+        codility::binaryGap(1041); 
     }
 }
-BENCHMARK(BM_BinaryGap);
+BENCHMARK(BM_BinaryGap_Small);
+
+// static void BM_BinaryGap_Large(benchmark::State& state) {
+//     for (auto _ : state) {
+//         codility::binaryGap(0xFFFFFFFF);  
+//     }
+// }
+// BENCHMARK(BM_BinaryGap_Large);
+
+
+static void BM_Rotate_Small(benchmark::State& state) {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    int k = 2;  
+    for (auto _ : state) {
+        auto result = codility::rotate(vec, k);
+    }
+}
+BENCHMARK(BM_Rotate_Small);
+
+// static void BM_Rotate_Large(benchmark::State& state) {
+//     std::vector<int> vec(1000, 1);  
+//     int k = 500; 
+//         for (auto _ : state) {
+//         auto result = codility::rotate(vec, k);
+//     }
+// }
+// BENCHMARK(BM_Rotate_Large);
 
 BENCHMARK_MAIN();
+
+
+// cmake -DCMAKE_BUILD_TYPE=Debug ..
+// cmake -DCMAKE_BUILD_TYPE=Release ..
+//cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O3" ..
